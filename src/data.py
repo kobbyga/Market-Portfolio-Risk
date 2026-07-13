b33da8dd187976c6b6cf6c1dbf4af169b5204ad0
kobbyga <Kobby.akuoko@outlook.com>
2026-07-13 15:17:08 +0100

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+from config.py import (
+    FX
+)
+
+
 def load_prices(path):
   """
   Loads DataFrame of csv file containing data, and computes log returns for all tickers
@@ -13,7 +18,19 @@ def load_prices(path):
   """
   df = pd.read_csv(path, parse_dates=["Dates"]).set_index("Dates").sort_index()
 
-  returns = np.log(df / df.shift(1)).dropna(how = "any")
+  # USD-denominated assets converted to GBP
+  usd_assets = ["NKE", "C", "SPX"]
+  for col in usd_assets:
+    df[col] = df[col] * df["USDGBP"]
 
-  return returns
+  # EUR-denominated assets converted to GBP
+  eur_assets = ["CBK", "VOW", "DAX"]
+  for col in eur_assets:
+    df[col] = df[col] * ( 1/ df["GBPEUR"])
+
+  df = df.drop(columns = FX)
+
+  # Compute log returns for assets 
+  returns = np.log(df / df.shift(1)).dropna(how="any")
   
+  return returns
