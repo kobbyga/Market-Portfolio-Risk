2a35ab2cab76e77fe85330a722865f22e55891aa
kobbyga <Kobby.akuoko@outlook.com>
2026-07-13 14:57:08 +0100

    Create data.py

---
+++ src/data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+def load_prices(path):
+  """
+  Loads DataFrame of csv file containing data, and computes log returns for all tickers
+
+  Args:
+      path: csv file containing close prices for all tickers
+
+  Returns:
+      returns: DataFrame containing log returns for all tickers
+  """
+  df = pd.read_csv(path, parse_date=["Dates"]).set_index("Dates").sort_index()
+
+  returns = np.log(df / df.shift(1)).dropna(how = "any")
+
+  return returns
+  
